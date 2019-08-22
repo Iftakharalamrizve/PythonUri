c03bb4fa519ab697381b02c5371f1606b8be244a
Iftakharalamrizve <iftakharalam32@gmailcom>
2019-08-22 10:52:33 +0600

    update

---
+++ uri145.py
@@ -0,0 +1,26 @@
+# x=int(input())
+# y=int(input())
+# i=1
+# while i<y:
+#     print("%d %d %d"%(i,(i+1),(i+1+1)))
+#     i=i+x
+
+# print("Good Morning!", end = '')
+# print("What a wonderful day!")
+
+#include <stdio.h>
+
+# int main()
+# {
+#     int order,range,i=1,j;
+#     scanf("%d %d",&order,&range);
+#     while(i<=range){
+#         for(j=0;j<order;j++){
+              #format=i;
+              #format=format+j;
+#             printf("%d ",format);
+#         }
+#         printf("\n");
+#         i=i+order;
+#     }
+# }
